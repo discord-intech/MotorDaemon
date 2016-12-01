1eabe1f6c40d428ae31a07ffffc04b2542ac8d35
discord <julian.desvignes@telecom-sudparis.eu>
2016-12-01 12:22:36 +0000

    constants

--- src/MotionController.cpp
+++ src/MotionController.cpp
@@ -58,8 +58,8 @@ averageLeftSpeed(), averageRightSpeed(), odo(67,68,44,26)
     toleranceDifferentielle = 500; // Pour les trajectoires "normales", v�rifie que les roues ne font pas nawak chacunes de leur cot�.
 
     translationPID.setTunings(0, 0, 0);
-    leftSpeedPID.setTunings(0.05, 0.0003, 0); // ki 0.00001
-    rightSpeedPID.setTunings(0.05, 0.0003, 0);
+    leftSpeedPID.setTunings(0.1, 0.0001, 0.0001); // ki 0.00001
+    rightSpeedPID.setTunings(0.1, 0.0001, 0.0001);
     curvePID.setTunings(0, 0, 0);
 
     distanceTest = 200;
