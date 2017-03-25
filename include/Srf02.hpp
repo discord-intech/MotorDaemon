080914b29b22d0a5014eb01e1e97787fc2612dd4
Xarboule (beagle) <remi.dulong@telecom-sudparis.eu>
2017-03-25 13:42:11 -0400

    Add srf02 library

---
+++ include/Srf02.hpp
@@ -0,0 +1,24 @@
+#ifndef SRF02_H_
+#define SRF02_H_
+#define SRF02_I2C_BUFFER 6
+
+
+class Srf02 {
+	private:
+		int I2CBus, I2CAddress;
+		char dataBuffer[SRF02_I2C_BUFFER];
+
+		int value;
+
+		int convertValue(unsigned int MSB, unsigned int LSB);
+	
+	public:
+		Srf02(int bus, int address);
+	
+		unsigned char get_i2c_register(unsigned char reg);	
+		int writeValue(unsigned char value);
+		int readValue();
+		int getValue();
+	};
+
+#endif
