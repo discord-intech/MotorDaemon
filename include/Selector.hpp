b7abda4ba9cb41c0740cb452ac53aca4984fad78
discord <julian.desvignes@telecom-sudparis.eu>
2017-04-28 14:31:21 +0200

    more info in status

--- include/Selector.hpp
+++ include/Selector.hpp
@@ -156,10 +156,14 @@ int treatOrder(std::string &order, std::function<void(char*)> print, bool proxyM
 
         long speed = motion.getSpeed();
 
+
+
         std::string s = std::string("MDSTATUS")+std::to_string(cpuU)+std::string(";")+
                 std::to_string(cpuT)+std::string(";")+std::to_string(sys.freeram)
                         +std::string(";")+std::to_string(sys.totalram)+std::string(";")+
-                        std::to_string(speed)+std::string("\r\n");
+                        std::to_string(speed)+std::string(";")+std::to_string(motion.getX())
+                        +std::string(";")+std::to_string(motion.getY())+std::string(";")+
+                        std::to_string(motion.getAngle())+std::string("\r\n");
 
 
 
