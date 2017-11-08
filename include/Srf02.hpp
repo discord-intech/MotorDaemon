a8bd37123b740c2cdd8092d0d5715827dd5e32ad
Remi Dulong <remi.dulong@telecom-sudparis.eu>
2017-11-09 00:33:39 +0100

    Add i2c-sensor library

--- include/Srf02.hpp
+++ include/Srf02.hpp
@@ -2,6 +2,17 @@
 #define SRF02_H_
 #define SRF02_I2C_BUFFER 6
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <iostream>
+#include "i2c-sensor.hpp"
+
+
 
 class Srf02 {
 	private:
@@ -11,11 +22,11 @@ class Srf02 {
 		int value;
 
 		int convertValue(unsigned int MSB, unsigned int LSB);
-	
+
 	public:
-		Srf02(int bus, int address);
-	
-		unsigned char get_i2c_register(unsigned char reg);	
+		Srf02(int bus, unsigned char address);
+
+		unsigned char get_i2c_register(unsigned char reg);
 		int writeValue(unsigned char value);
 		int readValue();
 		int getValue();
--- src/Srf02.cpp
+++ src/Srf02.cpp
@@ -1,12 +1,4 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <linux/i2c.h>
-#include <linux/i2c-dev.h>
-#include <sys/ioctl.h>
-#include <stdio.h>
 #include "Srf02.hpp"
-#include <iostream>
 
 using namespace std;
 
@@ -16,7 +8,7 @@ using namespace std;
 
 #define MAX_BUF 64
 
-Srf02::Srf02(int bus, int address){
+Srf02::Srf02(int bus, unsigned char address){
 	I2CBus = bus;
 	I2CAddress = address;
 	readValue();
@@ -24,86 +16,16 @@ Srf02::Srf02(int bus, int address){
 
 int Srf02::writeValue(unsigned char value) {
 
-	unsigned char addr = I2CAddress;
-	unsigned char reg = COMMAND_REG;
-
-	int file = I2CBus;
-
-	//int file = open("/dev/i2c-2", O_RDWR);
-
-	unsigned char outbuf[2];
-	struct i2c_rdwr_ioctl_data packets;
-	struct i2c_msg messages[1];
-
-	messages[0].addr  = addr;
-	messages[0].flags = 0;
-	messages[0].len   = sizeof(outbuf);
-	messages[0].buf   = outbuf;
-
-	/* The first byte indicates which register we'll write */
-	outbuf[0] = reg;
-
-	/* 
-	 *      * The second byte indicates the value to write.  Note that for many
-	 *           * devices, we can write multiple, sequential registers at once by
-	 *                * simply making outbuf bigger.
-	 *                     */
-	outbuf[1] = value;
-
-	/* Transfer the i2c packets to the kernel and verify it worked */
-	packets.msgs  = messages;
-	packets.nmsgs = 1;
-	if(ioctl(file, I2C_RDWR, &packets) < 0) {
-		perror("Unable to send data");
-		return 1;
-	}
-
-	return 0;
+	return i2c_write_value(I2CBus, I2CAddress, COMMAND_REG, value);
 }
 
 
 unsigned char Srf02::get_i2c_register(unsigned char reg) {
-	
-	
-	int file = I2CBus;
-	unsigned int addr = I2CAddress;
 
-	unsigned char inbuf, outbuf;
-	struct i2c_rdwr_ioctl_data packets;
-	struct i2c_msg messages[2];
-
-	/*
-	 *      * In order to read a register, we first do a "dummy write" by writing
-	 *           * 0 bytes to the register we want to read from.  This is similar to
-	 *                * the packet in set_i2c_register, except it's 1 byte rather than 2.
-	 *                     */
-	outbuf = reg;
-	messages[0].addr  = addr;
-	messages[0].flags = 0;
-	messages[0].len   = sizeof(outbuf);
-	messages[0].buf   = &outbuf;
-
-	/* The data will get returned in this structure */
-	messages[1].addr  = addr;
-	messages[1].flags = I2C_M_RD/* | I2C_M_NOSTART*/;
-	messages[1].len   = sizeof(inbuf);
-	messages[1].buf   = &inbuf;
-
-	/* Send the request to the kernel and get the result back */
-	packets.msgs      = messages;
-	packets.nmsgs     = 2;
-	if(ioctl(file, I2C_RDWR, &packets) < 0) {
-		perror("Unable to send data");
-		return 1;
-	}
-	
-	return inbuf;
+	return i2c_get_register(I2CBus, I2CAddress, reg);
 }
 
 int Srf02::readValue(){
-
-
-
 
 	this->value = convertValue(VALUE_MSB, VALUE_LSB);
 	return 0;
