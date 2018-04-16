91efc7f05bd39ea924c116436db95c4ab8efc3d6
julian.desvignes <julian.desvignes@telecom-sudparis.eu>
2018-04-16 22:04:14 +0200

    DEBUG

--- src/SerialController.cpp
+++ src/SerialController.cpp
@@ -163,14 +163,14 @@ void SerialController::readWorker()
             res->resultCode = js["code"].get<int>();
             memcpy(res->content, js["content"].get<std::string>().c_str(), js["content"].get<std::string>().length());
 
-//            if(res->resultCode)
-//            {
-//                std::cout << "ERROR CODE " << res->resultCode << " : " << res->content << std::endl;
-//            }
-//            else
-//            {
-//                std::cout << res->content << std::endl;
-//            }
+            if(res->resultCode)
+            {
+                std::cout << "ERROR CODE " << res->resultCode << " : " << res->content << std::endl;
+            }
+            else
+            {
+                std::cout << res->content << std::endl;
+            }
 
             resultQueue.push(res);
         }
